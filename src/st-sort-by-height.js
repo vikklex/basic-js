648611f5a7f6f094e11e6a0673da7c472405afdd
Viktoria <vikalex11@mail.ru>
2021-09-11 20:54:21 +0300

    fix: decide st-sum and st-sort

--- src/st-sort-by-height.js
+++ src/st-sort-by-height.js
@@ -11,7 +11,17 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-export default function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function sortByHeight(arr) {
+  let arrPlus = arr.filter((item) => item >= 0).sort((a,b) => a - b);
+  let arrMinus = arr.map((elm) => elm < 0 ? elm : '');
+  let result = []
+  for (let i = 0, j = 0; i < arr.length; i++) {
+    if(arrMinus[i] === '') {
+      result.push(arrPlus[j])
+      j+=1
+    }
+    else(result.push(arrMinus[i]))
+
+  }
+  return result
 }
--- src/st-sum-digits.js
+++ src/st-sum-digits.js
@@ -1,4 +1,4 @@
-import { NotImplementedError } from '../extensions/index.js';
+import { NotImplementedError } from "../extensions/index.js";
 
 /**
  * Given a number, replace this number with
@@ -12,7 +12,15 @@ import { NotImplementedError } from '../extensions/index.js';
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-export default function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getSumOfDigits(n) {
+  n = n.toString().split("").map(Number);
+  let res = n.reduce((total, item) => total + item);
+  while (res.toString().split("").length > 1) {
+    res = res
+      .toString()
+      .split("")
+      .map(Number)
+      .reduce((total, item) => total + item);
+  }
+  return res;
 }
