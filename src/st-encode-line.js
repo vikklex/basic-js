420b64d5c416378b07561359fe66a08ce6ea4619
Viktoria <vikalex11@mail.ru>
2021-09-11 22:12:12 +0300

    fix: deside st-encode-line

--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -10,7 +10,18 @@ import { NotImplementedError } from '../extensions/index.js';
  * For aabbbc should return 2a3bc
  *
  */
-export default function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function encodeLine(str) {
+  str = str.split('');
+  let res = [];
+  let count = 1;
+  for (let i = 0; i < str.length; i++) {
+    if(str[i] === str[i+1]) {
+      count++;
+    }
+    else {
+      count === 1 ? res.push('', str[i]) : res.push(count, str[i])
+      count = 1;
+    }
+  }
+  return res.join('');
 }
