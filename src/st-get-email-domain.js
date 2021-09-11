6f6cc300a40a4df186e16998fdb606155f477350
Viktoria <vikalex11@mail.ru>
2021-09-11 21:42:26 +0300

    fix: deside st-get-email

--- src/st-get-email-domain.js
+++ src/st-get-email-domain.js
@@ -10,7 +10,7 @@ import { NotImplementedError } from '../extensions/index.js';
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-export default function getEmailDomain(/* email */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getEmailDomain(email) {
+  let res = email.substring(email.lastIndexOf('@') +1);
+  return res
 }
