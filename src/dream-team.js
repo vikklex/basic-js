b2a6774cb4e3f4b876fa75d7387bb0690415abd9
Viktoria <vikalex11@mail.ru>
2021-09-12 11:48:37 +0300

    fix: deside dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,7 +13,22 @@ import { NotImplementedError } from '../extensions/index.js';
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function createDreamTeam(team) {
+  let arr = [];
+  let nick = [];
+  if (team === null || team === undefined || team.length === 0) {
+    return false
+  }
+  else {
+  for(let i = 0; i < team.length; i++) {
+    typeof(team[i]) === 'string' && team[i].length!== 0 ? arr.push(team[i].trim()) : arr
+  
+  }
+  let array = arr.map((el) => el.toUpperCase()).sort()
+  for(let i = 0; i < array.length; i++) {
+    nick.push(array[i][0])
+  }
+  let res = nick.join('').toUpperCase().trim();
+  return res
+}
 }
