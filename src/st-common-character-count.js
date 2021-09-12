e866a0af2c23ecbe4e7927d07ab5320f72f73987
Viktoria <vikalex11@mail.ru>
2021-09-12 12:38:12 +0300

    fix: deside common-character

--- src/st-common-character-count.js
+++ src/st-common-character-count.js
@@ -11,18 +11,13 @@ import { NotImplementedError } from '../extensions/index.js';
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-export default function getCommonCharacterCount(/*s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  
-  /*s1 = s1.split('');
-  let arr = new Set(s1);
-  let array = [...arr];
-  s2 = s2.split('');
+export default function getCommonCharacterCount(s1, s2) {
   let count = 0;
-  for(let i = 0; i < array.length; i++) {
-    s2.indexOf(array[i]) !== (-1) ? count++ : count
+  for (let i = 0; i < s1.length; i++) {
+    if (s2.indexOf(s1[i]) >= 0) {
+      count++
+      s2 = s2.replace(s1[i], '')
+    }
   }
   return count
-
-*/
 }
