81a474ff5cb8242c7693785cbbc8ad558bf9c645
Viktoria <vikalex11@mail.ru>
2021-09-13 15:49:58 +0300

    fix: decide transform task

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,28 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  if (!Array.isArray(arr))
+  throw Error("'arr' parameter must be an instance of the Array!");
+let res = [];
+for (let i = 0; i < arr.length; i++) {
+  switch (arr[i]) {
+    case "--discard-prev":
+      if (arr[i - 2] !== "--discard-next") res.pop();
+      break;
+    case "--discard-next":
+      i++;
+      break;
+    case "--double-next":
+      if (i + 1 < arr.length) res.push(arr[i + 1]);
+      break;
+    case "--double-prev":
+      if (i - 1 >= 0 && arr[i - 2] !== "--discard-next") res.push(arr[i - 1]);
+      break;
+    default:
+      res.push(arr[i]);
+  }
 }
+return res;
+}
+
