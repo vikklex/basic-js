e406f91948429c673271676cfe2289397ad7825f
Viktoria <vikalex11@mail.ru>
2021-09-14 10:37:52 +0300

    fix: deside recursive-depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -9,12 +9,20 @@ import { NotImplementedError } from '../extensions/index.js';
  * const depthCalc = new DepthCalculator();
  * depthCalc.calculateDepth([1, 2, 3, 4, 5]) => 1
  * depthCalc.calculateDepth([1, 2, 3, [4, 5]]) => 2
- * depthCalc.calculateDepth([[[]]]) => 3
+ * depthCalc.calculateDepth( [ [ [] ] ] ) => 3
  *
  */
 export default class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    let count = 1;
+    for (let i = 0; i < arr.length; i++) {
+      if (Array.isArray[arr[i]]) {
+        count += this.calculateDepth(arr.flat());
+        break;
+      }
+    }
+    return count
   }
 }
+
+
