bbff31c42153ac10de39a24f5589b0060a46cbdb
Viktoria <vikalex11@mail.ru>
2021-09-14 16:23:18 +0300

    fix: deside mac-adress task

--- src/st-mac-address.js
+++ src/st-mac-address.js
@@ -14,7 +14,9 @@ import { NotImplementedError } from '../extensions/index.js';
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-export default function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function isMAC48Address(n) {
+
+  let res = n.split('-');
+  return res.length === 6 && res.every((a) => /^[A-F-0-9]{2}$/.test(a))
+  
 }
