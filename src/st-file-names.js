858c68d9cabaab2915912d2843e3452e06cf77a5
Viktoria <vikalex11@mail.ru>
2021-09-14 16:07:52 +0300

    fix: deside file-names task

--- src/st-file-names.js
+++ src/st-file-names.js
@@ -15,7 +15,15 @@ import { NotImplementedError } from '../extensions/index.js';
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-export default function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function renameFiles(arr) {
+  for(let i = 0; i < arr.length; i++) {
+    let count = 1;
+    for(let j = i + 1; j < arr.length; j++) {
+      if(arr[i] === arr[j]) {
+        arr[j] = `${arr[j]}(${count})`
+        count++
+      }
+    }
+  }
+  return arr
 }
